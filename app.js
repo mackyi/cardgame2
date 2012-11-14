5498c33892ddcc11bdece4f65f3b1956320dfeff
Mack Yi <themackyi@gmail.com>
2012-11-13 20:18:41 -0500

    PORT

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
 app.use(express.static('public'));
-app.listen(3536);
-console.log('Listening on port 3536');
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+	console.log('Listening on port 3536');
+})
